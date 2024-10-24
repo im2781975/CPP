3c2145d49f809072942150786b984b86d3a08743
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-24 20:17:19 +0200

    Create 1.cpp

---
+++ 1000/1.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+//Find the minimum between difference between the largest & 2nd smallest and
+//the difference between the second largest & the smallest element.
+void FindMinDiff(){
+    int n; cin >> n;
+    vector <int> vec(n);
+    for(int i = 0; i < n; i++)
+        cin >> vec[i];
+    sort(vec.begin(), vec.end());
+    if(n < 2){
+        cout << "Not Enough Elements";
+        return;
+    }
+    cout << "Minimum Diff is: " << min((vec[n - 1] - vec[1]), (vec[n - 2] - vec[0]));
+}
