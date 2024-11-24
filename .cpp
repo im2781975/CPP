5d96afabf78aa8b8240714a4aeab3065c9f8e95e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-24 07:31:18 +0100

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,3 +1,91 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <bits/stdc++.h>
+using namespace std;
+
+void fileIO(void) {
+
+#ifndef ONLINE_JUDGE
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+#endif
+
+
+#ifdef ONLINE_JUDGE
+    //freopen("", "r", stdin);
+#endif
+
+}
+
+void fastIO(void) {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+}
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+
+typedef pair<ll, ll> pll;
+typedef pair<ll, pll> plll;
+
+typedef pair<ll, ld> pld;
+typedef pair<double, pii> pdii;
+
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+
+typedef vector<ll> vl;
+typedef vector<pll> vll;
+typedef vector<plll> vlll;
+
+typedef vector<string> vs;
+typedef vector<bool> vb;
+
+typedef vector<vl> vvl;
+
+typedef priority_queue<int> pq;
+typedef priority_queue<int, vector<int>, greater<int>> pqr;
+
+#define loop(n)          for(int i = 0; i < (n); i++)
+#define lop(n)           for(int j=0;j<(n);j++)
+#define lp(x, s, e)      for(int x = (s); x < (e); x++)
+#define lpe(x, s, e)     for(int x = (s); x <= (e); x++)
+#define loop_r(n)          for(ll i = (n) - 1; i >= 0; i--)
+#define lp_r(x, s, e)      for(ll x = (e) - 1; x >= (s); x--)
+#define lpe_r(x, s, e)     for(ll x = (e); x >= (s); x--)
+
+#define MP make_pair
+#define PB push_back
+#define EmB emplace_back
+#define Em emplace
+#define len(s)   (ll)s.length()
+#define sz(v)    (ll)v.size()
+#define all(a)   a.begin(),a.end()
+#define all_r(a)   a.rbegin(),a.rend()
+#define clr(x, val)    memset((x), (val), sizeof(x))
+#define maxEle *max_element
+#define minEle *min_element
+#define MP make_pair
+#define PB push_back
+#define len(s)   (int)s.length()
+#define sz(v)    (int)v.size()
+#define all(a)   a.begin(),a.end()
+#define all_r(a)   a.rbegin(),a.rend()
+#define clr(x, val)    memset((x), (val), sizeof(x))
+
+#define tests int t; cin >> t; int i = 0; while(i++ < t)
+
+#define endl '\n'
+#define escanor return 0
 #include <bits/stdc++.h>
 #define pb push_back
 #define ll long long
