a5fb52af95edf1edc2afba27bf71e39b970e32fa
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-26 15:24:19 +0100

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,3 +1,200 @@
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #VA_ARGS << "):", dbg_out(VA_ARGS)
+#else
+#define dbg(...)
+#endif
+#define ar array
+#define ll…
+#define all(cont) cont.begin(), cont.end()
+#define ll long long
+using namespace std;
+#define xx long long
+#define foi(xxxxx) for(xx yyyyy=0;yyyyy<xxxxx;yyyyy++)
+typedef vector<xx>          vll;
+#include<string>
+#include <sstream>
+#include<bitset>
+#define gc getchar_unlocked
+#define ll long long
+#define PI 3.1415926535897932384626
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define br printf("\n")
+#define fo(i, n) for(i=0;i<n;i++)
+#define fox(i, a,b) for(i=a;i<=b;i++)
+#define reps(i, a,b) for(int i=a;i<=b;i++)
+#define repe(i, a,b) for(int i=a;i>=b;i--)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define deb(x) cout << #x << " = " << x << endl;
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << endl
+#define deba(i, a, n…
+[10:29 pm, 23/01/2022] Pratik Suryawanshi: #include<bits/stdc++.h>
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt")
+#define gc getchar_unlocked
+#define ll long long int
+#define br printf("\n")
+#define fo(i, n) for(i=0;i<n;i++)
+#define fox(i, a,b) for(i=a;i<=b;i++)
+#define reps(i, a,b) for(int i=a;i<=b;i++)
+#define repe(i, a,b) for(int i=a;i>=b;i--)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define deb(x) cout << #x << " = " << x << endl;
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y;
+#include<bits/stdc++.h>
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#define google(tc) cout<<"Case #"<<tc++<<": ";
+#define FILE freopen("input.txt","r",stdin); freopen("output.txt","w", stdout);
+#define GetSetBolt ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); 
+#define ll long long int  
+#define LD long double
+#ifndef LOCAL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#endif
+#define max3(a,b,c) max(a,max(b,c)) 
+#define min3(a,b,c) min(a,min(b,c)) 
+#define FF first 
+#define SS second 
+#define PB push_back 
+#define PF push_front 
+#define PPB pop_back  
+#define PPF pop_front  
+#define Endl endl
+
+#define in(arr,n) for(int i=0;i<n;i++) cin>>arr[i];
+#define in2(arr,n,m) for(int i=0;i<n;i++){ for(int j=0;j<m;j++) cin>>arr[i][j];}
+#define dis(arr,n) for(int i=0;i<n;i++) cout<<arr[i]<<" "; cout<<endl;
+#define dis2(arr,n,m) for(int ii=0;ii<n;ii++){for(int j=0;j<m;j++)cout<<arr[ii][j]<<" ";cout<<endl;} 
+#define tc int t=0;cin>>t; while(t--) 
+
+#define For(n) for(ll i=0;i<n;i++)
+#define For0(x,z) for(ll x=0;x<z;x++)
+#define Forx(x,z) for(x;x<z;x++)
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+
+#define toLower(s) transform(s.begin(),s.end(),s.begin(),::tolower)
+#define toUpperr(s) transform(s.begin(),s.end(),s.begin(),::toupper)
+
+#define sortAD(arr,n) sort(arr,arr+n, greater<int>());
+#define sortVD(v) sort(v.begin(), v.end(), greater<int>());
+#define sortA(arr) sort(arr,arr+n);
+#define sortV(v) sort(v.begin(),v.end());
+
+#define mem0(X) memset((X), 0, sizeof((X)))
+#define memx(X,x)  memset((X), x, sizeof((X)))
+#define setbits(X)  __builtin_popcountll(X)
+#define precise(X)  cout<<fixed << setprecision(X);
+#define valid(x,y,row,col) (((x) >= 0 and (x) < row) and ((y) >= 0 and (y) < col))
+#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
+#define timer(d) for(long blockTime=NULL;(blockTime==NULL?(blockTime=clock())!=NULL:false); debug("%s:%.4fs",d,(double)(clock()-blockTime)/CLOCKS_PER_SEC))
+#define rsz resize
+#define bk back()
+#define ld long double
+typedef pair<int, int> PII; 
+typedef pair<ll, ll> PLL;
+typedef pair<double,double> PDD;
+typedef pair<string, string> PSS; 
+typedef pair<string, ll> PSL; 
+typedef long double lld;
+
+typedef vector<int> VI;  
+typedef vector<ll> VL;  
+typedef vector<double> VD;
+typedef vector<string> VS; 
+typedef vector<VI> VVI;  
+typedef vector<VL> VVL; 
+typedef vector<VS> VVS; 
+typedef vector<PII> VPII; 
+typedef vector<PLL> VPLL; 
+typedef vector<PSS> VPSS; 
+typedef vector<PSL> VPSL; 
+
+typedef map<int,int> MII; 
+typedef map<ll,ll> MLL;   
+typedef map<char,ll> MCL;  
+typedef map<char,int> MCI; 
+typedef map<char,ll> MCL;   
+typedef map<string,string> MSS;  
+typedef map<string,int> MSI;  
+typedef map<string,ll> MSL; 
+
+typedef unordered_map<int,int> UMII; 
+typedef unordered_map<ll,ll> UMLL;   
+typedef unordered_map<char,ll> UMCL;  
+typedef unordered_map<char,int> UMCI; 
+typedef unordered_map<char,ll> UMCL;   
+typedef unordered_map<string,string> UMSS;  
+typedef unordered_map<string,int> UMSI;  
+typedef unordered_map<string,ll> UMSL; 
+typedef unsigned long long ull;
+
+const int inf = (int)1e9 + 5;
+const ll infl = (ll)1e18 + 5;
+const ld PI = acos((ld)-1);
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 10;
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};   // for every grid problem
+
+
+//--------------------------------- Trace Functions --------------------------------------
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
 void fastio() {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
 #define ll long long
