9aed315a7d6fea9941e2b90dadbb4cb9375f7ad9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-06 06:36:24 +0100

    Update 1.cpp

--- 1000/1.cpp
+++ 1000/1.cpp
@@ -1,5 +1,5 @@
 using namespace std;
-//Find the minimum between difference between the largest & 2nd smallest and
+//Find the minimum difference between the largest & 2nd smallest and
 //the difference between the second largest & the smallest element.
 void FindMinDiff(){
     int n; cin >> n;
@@ -12,4 +12,24 @@ void FindMinDiff(){
         return;
     }
     cout << "Minimum Diff is: " << min((vec[n - 1] - vec[1]), (vec[n - 2] - vec[0]));
+}
+using namespace std;
+//determines the minimum number of elements needed such that 
+//their sum is greater than the sum of the remaining elements in the vector.
+void CntElements(){
+    int n; cin >> n;
+    int sum = 0, cnt = 0, tmp = 0;
+    vector <int> vec;
+    for(int i = 0; i < n; i++){
+        int val; cin >> val;
+        vec.push_back(val);
+        sum += val;
+    }
+    sort(vec.begin(), vec.end());
+    for(int i = n - 1; i >= 0; i--){
+        tmp += vec[i]; cnt++;
+        if(tmp > (sum - tmp))
+            break;
+    }
+    cout << "Maximum Sum elements Count are: " << cnt;
 }
