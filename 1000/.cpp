133bd6279fbbfc366e6c5d9d21bc2e7a2c95d451
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-07 07:27:07 +0100

    Update .cpp

--- 1000/.cpp
+++ 1000/.cpp
@@ -396,3 +396,13 @@ void MinimizeCost(){
     else
         cout << min(tmp1, tmp2);
 }
+using namespace std;
+int Fact(int n){
+    if(n == 0 || n == 1)
+        return 1;
+    return n * Fact(n - 1);
+}
+int main(){
+    int n; cin >> n;
+    cout << Fact(n);
+}
