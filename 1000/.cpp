cf2c28cc044fafc921b88875484e3574b055a015
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-10 08:55:01 +0100

    Update .cpp

--- 1000/.cpp
+++ 1000/.cpp
@@ -957,3 +957,32 @@ int main(){
     vector <vector <int> >dp(n, vector <int> (r + 1, 1))
     cout << fact(n, r, dp);
 }
+using namespace std;
+void BubbleSort(){
+    int n; cin >> n;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] < arr[i + 1])
+            continue;
+        else
+            cnt = -1;
+    }
+    if(cnt == 0){
+        cout << "Ordered";
+        return 0;
+    }
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - 1- i; i++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr[j], arr[j + 1]);
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << "\n";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
