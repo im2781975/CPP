1f4fde5f1178088c08b27af9887602e97ba458db
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-14 11:50:58 +0100

    Update .cpp

--- 1000/.cpp
+++ 1000/.cpp
@@ -285,3 +285,315 @@ void cntGrpAvg(){
         }
     }
 }
+using namespace std;
+//construct a binary tree from user input
+vector <int> vec(100, 0);
+int create(int i){
+    int x; cin >> x;
+    if(x == 0)
+        return 0;
+    vec[i] = x;
+    create(2 * i);
+    create(2 * i + 1);
+    return x;
+}
+void func(int i){
+    if(i >= vec.size() || vec[i] == 0)
+        return;
+    func(i * 2);
+    cout << vec[i] << " ";
+    func(i * 2 + 1);
+}
+int main(){
+    create(1); func(1);
+    string str(1, char(127));
+    cout << str;
+}
+using namespace std;
+int letterCnt(int n){
+    string unit[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    string teens[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    string tens[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    string hundred = "hundred";
+    int cnt = 0;
+    if(n >= 100){
+        cnt += unit[n / 100].length() + hundred.length();
+        if(n % 100 != 0)
+            cnt += 3; //for 'and'
+        n %= 100;
+    }
+    if(n >= 20){
+        cnt += tens[n / 10].length();
+        n %= 10;
+    }
+    if(n >= 10)
+        cnt += teens[n - 10].length();
+    else
+        cnt += unit[n].length();
+    return cnt;
+}
+int main(){
+    int digit = 11; //one thousand;
+    for(int i = 1; i <= 999; i++)
+        digit += letterCnt(i);
+    cout << "Total letter: " << digit;
+    cout << letterCnt(911);
+}
+using namespace std;
+int power(int numer, int denom, int mod){
+    if(denom == 0)
+        return 1;
+    if(denom % 2 == 0){
+        int half = power(numer, denom / 2, mod);
+        return (half * half) % mod;
+    }
+    else
+        return (numer * power(numer, denom - 1, mod)) % mod;
+}
+int main(){
+    int numer, denom, mod; cin >> numer >> denom >> mod;
+    cout << power(numer, denom, mod);
+}
+using namespace std;
+//Access elements from spiral
+void spiral(){
+    int row, col, res; cin >> row >> col;
+    if(row > col){
+        if(row % 2 == 0)
+            res = (row * row) - col + 1;
+        else
+            res = (row - 1) * (row - 1) + col;
+    }
+    else{
+        if(col % 2 == 0)
+            res = (col - 1) * (col - 1) + row;
+        else
+            res = (col * col) - row + 1;
+    }
+    cout << res;
+}
+using namespace std;
+//identify if a number power of two or not
+void poweredOfTwo(){
+    int n; cin >> n;
+    // [n & (n - 1)] rep binary bitset.a power of 2 rep only one bitset.(100 & 011 = 000)
+    //(n > 0 && (n & (n - 1)) == 0)? cout << "Yes" : cout << "No";
+    for(int i = 0; i <= n; i++){
+        if(pow(2, i) == n){
+            cout << "Yes";
+            return 0;
+        }
+    }
+    cout << "No";
+}
+//check is number contain all unique int
+using namespace std;
+void IsUnique(){
+    int n; cin >> n;
+    int digit[4];
+    if(n < 1000 && n > 9999){
+        cout << "Invalid";
+        return 0;
+    }
+    //(1234 / 10^i) % 10 return the last digits.
+    for(int i = 0; i < 4; i++)
+        digit[i] = (n / static_cast < int >(pow(10, i))) % 10;
+    for(int i = 0; i < 4; i++){
+        for(int j = i + 1; j < 4; j++){
+            if(digit[i] == digit[j]){
+                cout << "Not Unique";
+                return 0;
+            }
+        }
+    }
+    cout << "unique";
+}
+using namespace std;
+void makePalindrome(){
+    int len; cin >> len;
+    char arr[len + 1];
+    int l = 0, r = len - 1, cnt = 0;
+    while(l < r){
+        if(arr[l] !=  arr[r]){
+            cnt++;
+            if(arr[l] < arr[r])
+                arr[r] = arr[l];
+            else
+                arr[l] = arr[r];
+        }
+        l++; r--;
+    }
+    cout << change;
+}
+using namespace std;
+void IsPalindrome(){
+    int n; cin >> n;
+    if(n < 0)
+        return 0;
+    if(n == 0 || n == 1){
+        cout << "palindrome";
+        return 0;
+    }
+    int arr[n], cnt = 0;
+    while(n != 0){
+        arr[cnt++] = n % 10;
+        n /= 10;
+    }
+    int i = 0, j = cnt - 1;
+    while(i < j){
+        if(arr[i] != arr[j]){
+            cout << "Not palindrome";
+            return 0;
+        }
+        i++; j--;
+    }
+    cout << "palindrome;"
+}
+using namespace std;
+bool Ispalindrome(int *arr, int idx, int n){
+    if(idx >= n / 2)
+        return true;
+    return (arr[idx] == arr[n - 1 - idx]) && Ispalindrome(arr, idx + 1, n);
+}
+int main(){
+    int arr[] = {1, 2, 3, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    (Ispalindrome(arr, 0, n) ? cout << "Yes" : cout << "No");
+}
+using namespace std;
+//checks if n can be decomposed into a sum of two even integers.If n is even and greater than 2,  prints "YES", else prints "NO".
+void DivideEven(){
+    int n; cin >> n;
+    if(n % 2 == 0 && n != 2)
+        cout << "Yes";
+    if(n % 2 == 1 || n == 2)
+        cout << "No";
+}
+using namespace std;
+// reads pairs of hex numbers, computes their sum, and outputs the result in uppercase hex format
+void convertToHex(){
+    int a, b;
+    // hex manipulator tells to interpret input values as hexadecimal numbers.
+    while(cin >> hex >> a >> b){
+        int sum = a + b;
+        cout << hex << uppercase;
+        if(sum < 0)
+            cout << "-" << -sum;
+        else
+            cout << sum;
+        //setiosflags(uppercase): Ensures that hexadecimal letters (A–F) are displayed in uppercase.
+        /*if(a + b < 0)
+            cout << setiosflags(uppercase) << hex << "-" << -(a + b);
+        else
+            cout << setiosflags(uppercase) << a + b;
+        cout << "\n";*/
+    }
+}
+using namespace std;
+void pairSum(){
+    int a, b; cin >> a >> b;
+    //((a + b) % 3 == 0 || a <= 2 * b || b <= 2 * a)? cout << "Yes" : cout << "No";
+    while(true){
+        if(a == 0 && b == 0)
+            break;
+        else if((a + b) % 3 != 0)
+            break;
+        if((a % 3 == 0) && (b % 3 == 0)){
+            a = 0; b = 0;
+            break;
+        }
+        else{
+            if(a > b)
+                a -= 2; b -= 1;
+            else
+                a -= 1; b -= 2;
+        }
+    }
+    (a == 0 && b == 0)? cout << "Yes" : cout << "No";
+}
+using namespace std;
+void statement(){
+    int n; cin >> n;
+    (n == 2)? cout << 2 : cout << 2 * n - 4;
+}
+using namespace std;
+void statement(){
+    int x, y; cin >> x >> y;
+    if((2 * x - y) >= 0 && (2 * x - y) % 3 == 0 && (2 * y - x) >= 0 && (2 * y - x) % 3 == 0)
+        cout << "Yes";
+    else
+        cout << "No";
+}
+
+using namespace std;
+//determine how many iterations will take for the value of 'a' to become greater than or
+//equal to the value of 'b' when a is tripled and b is doubled in each iteration.
+void countIteration(){
+    int a, b; cin >> a >> b;
+    int cnt = 0;
+    while(a < b){
+        a *= 3; b *= 2;
+        cnt++;
+    }
+    cout << cnt;
+}
+using namespace std;
+//Find Minimum & diffrence between sum of these number & twice of minimum
+void MinDiff(){
+    int a, b, c; cin >> a >> b;
+    if(a <= b) c = a;
+    else c = b;
+    cout << "Min val is: " << c << " Expression is: " << (a + b - 2 * c);
+}
+using namespace std;
+//After some specific operation in k times whats the value of n 
+void wrongSubtract(){
+    int n, k; cin >> n >> k;
+    for(int i = 0; i < k; i++){
+        if(n % 10 == 0)
+            n /= 10;
+        if(n % 10 != 0)
+            n--;
+    }
+    cout << n;
+}
+using namespace std;
+void sumInt(){
+    int a, b, res = 0; char ch;
+    while(cin >> ch && ch != 'e'){
+        cin >> a >> b;
+        res += a + b;
+    }
+    cout << res;
+}
+using namespace std;
+void seive(){
+    const int maxi = 1e7;
+    bool prime[maxi];
+    prime[0] = prime[1] = false;
+    for(int i = 2; i <= maxn; i++)
+        prime[i] = true;
+    for(int i = 2; i <= maxn; i++){
+        if(prime[i]){
+            for(int j = i * i; j <= maxn; j += i)
+                prime[j] = false;
+        }
+    }
+    for(int i = 2; i <= maxn; i++){
+        if(prime[i]){
+            cout << i << " ";
+        }
+    }
+}
+using namespace std;
+//determine if the count of 'lucky' digits (4 and 7) in a given number n is exactly 4 or exactly 7. 
+void Lucky(){
+    int n, cnt = 0; cin >> n;
+    while(n != 0){
+        int dig = n % 10;
+        n /= 10;
+        if(dig == 4 || dig == 7)
+            cnt++;
+    }
+    (cnt == 4 || cnt == 7) ? cout << "Yes" : cout << "No";
+}
