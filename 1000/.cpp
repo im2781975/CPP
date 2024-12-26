2baed24f2c21a7a183446899115bf4bc53609260
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-12-26 08:31:51 +0100

    Update .cpp

--- 1000/.cpp
+++ 1000/.cpp
@@ -846,3 +846,561 @@ int main(){
             cout << -1;
     }
 }
+using namespace std;
+void Collatz Conjecture(){
+    int n, x; cin >> n >> x;
+    for(int i = 0; i < n; i++){
+        (x % 2 == 0) ? x /= 2 : x = 3 * x + 1;
+        cout << x << " ";
+    }
+}
+using namespace std;
+//if the charge is less than 60% will take 1 minute to charge for every 1%. If the charge is less than 80%  will take 2 minute 
+// And finally from 80% it will take 3 minutes .if the current charge is X% then how many minutes it will take to charge completely.
+void cntMinute(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    int minute[n];
+    if(arr[i] >= 0 && arr[i] < 60)
+        minute[i] = (60 - arr[i]) * 1;
+    else if(arr[i] >= 60 && arr[i] < 80)
+        minute[i] = (80 - arr[i]) * 2 + 40;
+    else if(arr[i] >= 80 && arr[i] <= 100)
+        minute[i] = (100 - arr[i]) * 3 + 140;
+    for(int i = 0; i < n; i++)
+        cout << minute[i] << " ";
+}
+using namespace std;
+// determine how many 01 and 10 pair exist in the a string.
+void pairMatch(){
+    int n; cin >> n;
+    string str; cin >> str;
+    int cnt = 0;
+    for(int i = 0, j = 1; str[j]; i++, j++){
+        if((str[i] == '1' && str[j] == '0') || (str[i] == '0' && str[j] == '1')
+            cnt++;
+    }
+    cout << cnt;
+}
+using namespace std;
+void binarySearch(){
+    vector <int> vec {2, 6, 8, 10, 12, 14, 15, 18, 19, 28, 30};
+    int low = 0, high = vec.size() - 1;
+    int trg = 15, res = -1;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(arr[mid] == trg){
+            res = mid;
+            break;
+        }
+        else if(arr[mid] > trg)
+             high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    cout << res;
+}
+using namespace std;
+void Diffrence(){
+    int a, b; cin >> a >> b;
+    if(a >= 0 && b >= 0 && a <= 100 && b <= 100){
+        int dist = abs(a - b)
+        cout << dist;
+    }
+}
+using namespace std;
+void pattern(){
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = i; j >= 1; j--)
+            cout << j << " ";
+        cout << "\n";
+    }
+}
+// divide N among 3 people such that every person get the same amount. If possible give same amount to 3 people, print -1 otherwise print the amount.
+void divideAgain(){
+    int x; cin >> x;
+    (x % 3 == 0)? cout << x / 3 ; cout << -1;
+}
+using namespace std;
+//captain put the notebooks of girls first, and then the boys. captain should keep the boy's notebooks to the left side and then the girl's notebooks. 
+//If the last index of a girl's notebook is K then can you print them in the right order? Note: If K is 0, that means there is no girl's notebook.
+void divideThem(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    int m; cin >> m;
+    //girls notebook
+    for(int i = m; i <= n; i++)
+        cout << arr[i] << " ";
+    //boys botebook
+    for(int i = 0; i < m; i++)
+        cout << arr[i] << " ";
+}
+using namespace std;
+void fifaYear(){
+    int year; cin >> year;
+    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        cout << "Yes";
+    else
+        cout << "No";
+}
+using namespace std;
+void FirstLastIdx(){
+    vector <int> arr{1, 2, 3, 3, 3, 4, 5, 6};
+    int low = 0, high = arr.size() - 1;
+    int trg = 3, firstIdx = -1;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(arr[mid] == trg){
+            firstIdx = mid;
+            high = mid - 1;
+        }
+        else if(arr[mid] > trg)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    low = 0, high = arr.size() - 1;
+    int lastIdx = -1;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(arr[mid] == trg){
+            lastIdx = mid;
+            low = mid + 1;
+        }
+        else if(arr[mid] > trg)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    cout << firstIdx << " " << lastIdx;
+}
+using namespace std;
+// given an array of N positive integers. Find the maximum even value from the array. For finding, can perform only one of the following operations-
+//Choose any even value from the array.
+// Choose any two(must be different) values from the array and perform summation on those two values. If it will even you can pick those two values.
+void pickValue(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] > arr[j]){
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    int res = ans = 0;
+    int evenCnt = oddCnt = 2;
+    for(int i = n - 1; i >= 0; i--){
+        if(arr[i] % 2 == 0 && evenCnt > 0){
+            evenCnt--;
+            res += arr[i];
+        }
+        else if(arr[i] % 2 == 1 && oddCnt > 0){
+            oddCnt--;
+            ans += arr[i];
+        }
+    }
+    if(oddCnt > 0)
+        cout << res;
+    else
+        cout << max(res, ans);
+}
+using namespace std;
+//Count the total unique characters from the string
+void cntUnique(){
+    int arr[26] = {0}, cnt = 0;
+    string str; cin >> str;
+    int i = 0;
+    while(str[i] != '\0'){
+        if(str[i] >= 'a' && str[i] <= 'z')
+            arr[str[i] - 'a']++;
+        i++;
+    }
+    for(int i = 0; i < 26; i++){
+        if(arr[i] > 0)
+            cnt++;
+    }
+    cout << cnt;
+}
+using namespace std;
+//count the total unique hand shakes
+void cntHandShakes(){
+    int n; cin >> n;
+    int res = (n * (n - 1)) / 2;
+    cout << res;
+}
+using namespace std;
+//reorder a string letters in such a way that it becomes a palindrome
+void createPalindrome(){
+    string str;
+    cin >> str;
+    int n = str.length();
+    vector<int> freq(27, 0);
+    for (char ch : str){
+        freq[ch - 'A' + 1]++;
+
+    int oddValue = -1, totalCharCount = 0, odd = 0;
+    char oddChar;
+
+    for (int i = 1; i <= 26; ++i){
+        if (freq[i] >= 1)
+            totalCharCount++;
+        if (freq[i] % 2 != 0){
+            oddValue = freq[i];
+            oddChar = i + 64;
+            freq[i] = 0;
+            odd++;
+        }
+    }
+    // If more than one odd frequency, no solution
+    if (odd > 1){
+        cout << "NO SOLUTION" << endl;
+        return 0;
+    }
+    int remainingChar = n;
+    if (oddValue != -1){
+        totalCharCount -= 1;
+        remainingChar -= oddValue;
+    }
+    string firstHalf, secondHalf;
+    // Create the palindrome halves
+    for (int i = 1; i <= 26; ++i){
+        if (freq[i] > 1){
+            char evenChar = i + 64;
+            int halfCount = freq[i] / 2;
+            firstHalf.append(halfCount, evenChar);
+            secondHalf.append(halfCount, evenChar);
+        }
+    }
+    // Reverse the second half
+    reverse(secondHalf.begin(), secondHalf.end());
+    // Print the result
+    cout << firstHalf;
+    if (oddValue != -1){
+        cout << string(oddValue, oddChar);
+    }
+    cout << secondHalf;
+    return 0;
+}
+using namespace std;
+void xossPatern(){
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++){
+        cout << i;
+        for(int j = 2; j <= n; j++){
+            if(i == 1)
+                cout << j;
+            else if(i < n){
+                if(j == n)
+                    cout << n;
+                else
+                    cout << " ";
+            }
+            else
+                cout << n;
+        }
+        cout << "\n";
+    }
+}
+using namespace std;
+bool Isvalid(){
+    string str; cin >> str;
+    stack <char> st;
+    for(int i = 0; i < str.size(); i++){
+        char ch = str[i];
+        if(ch == '(' || ch == '{' || ch == '[')
+            st.push(ch);
+        else if(ch == ')' && !st.empty() && st.top() == '(')
+            st.pop();
+        else if(ch == '}' && !st.empty() && st.top() == '{')
+            st.pop();
+        else if(ch == ']' && !st.empty() && st.top() == '[')
+            st.pop();
+        else
+            st.push(ch);
+    }
+    (st.empty())? cout << "valid" : cout << "Invalid";
+}
+using namespace std;
+bool Isvalid(string str){
+    int n = str.size();
+    bool found = true;
+    vector <int> vec();
+    for(int i = 0; i < n; i++){
+        if((int)vec.size() == 0)
+            vec.push_back(str[i]);
+        else{
+            char ch = vec.back();
+            if(ch == '{' && str[i] == '}')
+                vec.pop_back();
+            else if(ch == '[' && str[i] == ']')
+                vec.pop_back();
+            else if(ch == '(' && str[i] == ')')
+                vec.pop_back();
+            else
+                vec.push_back(str[i]);
+        }
+    }
+    found = (vec.size() == 0) ? true : false;
+    return found;
+}
+#include<bits/stdc++.h>
+using namespace std;
+// If b is added to twice a number, the sum will be sum. What is the number
+void TwinNumber(){
+    int sum, a, b; cin >> sum >> a;
+    b = sum - a;
+    cout << b / 2;
+}
+using namespace std;
+//If a value of a matrix matches with the current row and column  add 3.
+//If matches only the current row then add 2, If matches only the current column number then add 1 with the value.
+void 2DOperation(){
+    int row, col; cin >> row >> col;
+    int arr[101][101];
+    for(int i = 1; i <= row; i++){
+        for(int j = 1; j <= col; j++)
+            cin >> arr[i][j];
+    }
+    for(int i = 1; i <= row; i++){
+        for(int j = 1; j <= col; j++){
+            if(i == arr[i][j] && j == arr[i][j])
+                arr[i][j] += 3;
+            else if(i == arr[i][j])
+                arr[i][j] += 2;
+            else if(j == arr[i][j])
+                arr[i][j] += 1;
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+using namespace std;
+//return the square root of n rounded down the nearest integers
+void sqrot(){
+    int n; cin >> n;
+    int low = 0, high = n - 1, res;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(mid * mid <= n){
+            res = mid;
+            low = mid + 1;
+        }
+        if(mid * mid > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    cout << res;
+}
+using namespace std;
+void searchVal(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    int trg; cin >> trg;
+    int idx = -1;
+    for(int i = 0; i < n; i++){
+        if(arr[i] == trg){
+            idx = i;
+            break;
+        }
+    }
+    (idx!= -1) ? cout << idx + 1 : cout << "Not Found";
+}
+using namespace std;
+// a number would be a good secret code if no other number except 1 and N can divide that number. find out that number
+void secretCode(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < n; i++){
+        int cnt = 0;
+        if(arr[i] == 0 || arr[i] == 1)
+            cnt++;
+        for(int j = 2; j <= (arr[i] / 2); j++){
+            if(arr[i] % j == 0){
+                cnt++;
+                break;
+            }
+        }
+        (cnt == 0) ? cout << "Yes" : cout << "No";
+    }
+}
+using namespace std;
+void ReversePrint(){
+    int n; cin >> n;
+    stack <int> st;
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        st.push(x);
+    }
+    while(!st.empty()){
+        cout << st.top() << " ";
+        st.pop();
+    }
+}
+using namespace std;
+//each number [1 – 9] represent [A – I].But if the number is more than one digit it reverse itself.
+void RevCode(){
+    int n; cin >> n;
+    while(n > 0){
+        cout << char(64 + n % 10);
+        n /= 10;
+    }
+}
+using namespace std;
+//given signs(plus or minus) consecutively. not one by one.count maximum of all signs which give sequentially
+void plusMinus(){
+    int n; cin >> n;
+    string str; cin >> str;
+    int cnt = 1, maxi = 1;
+    for(int i = 1; i < n; i++){
+        if(str[i] == str[i - 1])
+            cnt++;
+        else
+            cnt = 1;
+        if(maxi <= cnt)
+            maxi = cnt;
+    }
+    cout << maxi;
+}
+using namespace std;
+int peakIdx(vector <int> &vec){
+    int res;
+    for(int i = 0; i < vec.size(); i++){
+        if(vec[i] > vec[i - 1] && vec[i] > vec[i + 1]){
+            res = i;
+            break;
+        }
+    }
+    cout << res;
+}
+using namespace std;
+//If any player isn't able to make at least K runs he will be out of the team. Count number of players that will be out of the team
+void outOfTeam(){
+    int player, run; cin >> player >> run;
+    int arr[player], cnt = 0;
+    for(int i = 0; i < player; i++)
+        cin >> arr[i];
+    for(int i = 0; i < player; i++){
+        if(arr[i] < run)
+            cnt++;
+    }
+    cout << cnt;
+}
+using namespace std;
+// given a string consisting Add, Mult. next line will contain two positive values. Calculate the sum of every operations. 
+void Operation(){
+    string str; cin >> str;
+    int a, b; cin >> a >> b;
+    int res = 0;
+    for(int i = 0; str[i]; i++){
+        if(str[i] == '+')
+            res = a + b;
+        else if(str[i] == '*')
+            res = a * b;
+    }
+    cout << res;
+}
+using namespace std;
+const int N = 300;
+void MissingSeqNum(){
+    int n; cin >> n;
+    int arr[N];
+    for(int i = 1; i < n; i++){
+        int x; cin >> x;
+        arr[x] = 1;
+    }
+    for(int i = 1; i <= n; i++){
+        if(arr[i] != 1)
+            cout << i << " ";
+    }
+}
+using namespace std;
+// Given the sum of four numbers and three of those numbers, find the missing number. All four numbers are positive integers
+void MissingNum(){
+    int sum, a, b, c; cin >> sum >> a >> b >> c;
+    int res = sum - (a + b + c);
+    cout << res;
+}
+using namespace std;
+//If a word isn't a palindrome, print “Not Palindrome”.elseIfword is too long if its and length more than 7 characters.
+//All too long words should be replaced with a special abbreviation.if it is plaindrome and length is less than or equal 7 characters.Then, print the word
+void Ispalindrome(){
+    string str; cin >> str;
+    int IsPalindrome = 1, len = str.size();
+    for(int i = 0; i < len / 2; i++){
+        if(str[i] != str[len - 1 - i]){
+            IsPalindrome = 0;
+            break;
+        }
+    }
+    if(IsPalindrome){
+        if(len > 7)
+            cout << str[0] << " " << len - 2 << " " << str[len - 1];
+        else
+            cout << str;
+    }
+    else
+        cout << "Not Palindrome";
+}
+using namespace std;
+//Find the highest marks.and print the diffrence between another marks
+void HighestMark(){
+    int n; cin >> n;
+    int mark[n];
+    for(int i = 0; i < n; i++)
+        cin >> mark[i];
+    int tmp = mark[0];
+    for(int i = 1; i < n; i++){
+        if(tmp < mark[i])
+            tmp = mark[i];
+    }
+    for(int i = 0; i < n; i++){
+        cout << tmp - mark[i] << " ";
+    }
+}
+using namespace std;
+//modify an array so that it is increasing.every element at least as large as previous one.
+//one each move you can increase the value of any element by one.what is the minimum move required.
+void Incrarr(){
+    int n; cin >> n;
+    int arr[n], res = 0;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 1; i < n; i++){
+        if(arr[i] < arr[i - 1])
+            res += arr[i - 1] - arr[i];
+    }
+    cout << res;
+}
+using namespace std;
+bool IsPalindrome(string str){
+    int n = str.size();
+    int l = 0, r = n - 1;
+    while(l < r){
+        if(str[l] != str[r])
+            return false;
+        l++; r--;
+    }
+    return true;
+}
+bool IsPalindrome(string str){
+    if(str == " " || str.size() == 1)
+        return true;
+    int n = str.size();
+    string ing = str.substr(1, n - 1);
+    return IsPalindrome(ing) && (str[0] == str.back())b;
+}
