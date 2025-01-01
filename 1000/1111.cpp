75e52da097ae5759023f7019e2854c645e3921cd
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-01 10:04:48 +0600

    Update 1111.cpp

--- 1000/1111.cpp
+++ 1000/1111.cpp
@@ -1982,3 +1982,134 @@ int main(){
     for(int i = 0; i < fac.size(); i++)
         cout << fac[i] << " ";
 }
+#include<bits/stdc++.h>
+using namespace std;
+int finish[10],TAT[10],WAT[10],star[10];
+struct node{
+    int name;
+    int arrive;
+    int burst;
+}arr[10];
+bool cmp(node A, node B)
+{
+    if( A.arrive == B.arrive)
+        return A.name < B.name;
+    return A.arrive < B.arrive;
+}
+
+void FCFS(int N)
+{
+    for(int i=0;i<N;i++)
+    {
+        if(i==0)
+            star[i]=arr[i].arrive;
+        else
+            star[i]=finish[i-1];
+
+        WAT[i]=star[i]-arr[i].arrive;
+        finish[i]=star[i]+arr[i].burst;
+        TAT[i]=finish[i]-arr[i].arrive;
+    }
+
+    double sum=0,sum1=0;
+    for(int i = 0 ; i < N ; i++){
+        sum += (double)WAT[i];
+        sum1 += (double)TAT[i];
+    }
+
+    printf("Avg TAT : %f\n",sum1/(double)N);
+    printf("Avg WAT : %f\n",sum/(double)N);
+
+}
+int main()
+{
+    int N;
+    cin>>N;
+
+    for(int i = 0; i < N; i++){
+        arr[i].name = i+1;
+        cin>>arr[i].arrive >> arr[i].burst;
+    }
+
+    sort(arr,arr+N,cmp);
+    FCFS(N);
+}
+
+#include<bits/stdc++.h>
+using namespace std;
+int Arr[100005], Tree[400005] , Lazy[400005];
+void init(int node, int S, int E)
+{
+    if(S == E){
+        Tree[node] = Arr[S];
+        return;
+    }
+
+    int L = 2*node;
+    int R = L+1;
+    int mid = (S+E)/2;
+
+    init(L, S, mid);
+    init(R, mid+1, E);
+    Tree[node] = Tree[L] + Tree[R];
+}
+
+void update(int node, int S, int E, int i, int value)
+{
+    if(i>E || i <S)
+        return;
+    if(S>=i && E<= i){
+        Tree[node] = value;
+        return;
+    }
+
+
+    int L = 2*node;
+    int R = L+1;
+    int mid = (S+E)/2;
+
+    update(L, S, mid , i , value);
+    update(R, mid+1, E , i , value);
+    Tree[node] = Tree[L] + Tree[R];
+}
+int query(int node, int S, int E, int i, int j)
+{
+    if(i>E || j < S)
+        return 0;
+    if(i<=S && j >= E)
+        return Tree[node];
+
+
+    int L = 2*node;
+    int R = L+1;
+    int mid = (S+E)/2;
+
+    int q1 = query(L, S, mid , i , j);
+    int q2 = query(R, mid+1, E , i , j);
+    return q1+q2;
+}
+
+int main()
+{
+    int n,q;
+    cin>>n>>q;
+
+    init(1,1,n);
+
+    while(q--)
+    {
+        int l;
+        cin>>l;
+
+        if(l == 1){
+            int pos, v;
+            cin>>pos>>v;
+            update(1,1,n,pos,v);
+        }
+        else{
+            int a,b;
+            cin>>a>>b;
+            cout<<query(1,1,n,a,b)<<endl;
+        }
+    }
+}
