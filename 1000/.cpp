3493d3e141be0946eec95fef519f78680e652de1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-06 12:18:25 +0600

    Update .cpp

--- 1000/.cpp
+++ 1000/.cpp
@@ -522,3 +522,539 @@ int main(){
     }
     cout << ans;
 }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num, maxDiff, Query; cin >> num >> maxDiff >> Query;
+    vector <pair <int, int> >twin(num + 1);
+    for(int i = 1; i <= num; i++){
+        cin >> twin[i].first;
+        twin[i].second = i;
+    }
+    sort(twin.begin() + 1, twin.end());
+    priority_queue <pair <int, int> > pq;
+    vector <int> res(num + 1, 0);
+    for (int i = 1, j = 1; i <= num; i++) {
+        while (j <= num && twin[j].first - twin[i].first <= maxDiff)
+            j++;
+        j--;
+        pq.push({j - i + 1, j});
+        while (pq.top().second < i)
+            pq.pop();
+        res[twin[i].second] = pq.top().first;
+    }
+    for (int i = 0; i < Query; i++) {
+        int idx; cin >> idx;
+        cout << res[idx] << endl;
+    }
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    string str; cin >> str;
+    vector <int> vec(26, 0);
+    for(int i = 0; i < n; i++)
+        vec[str[i] - 'A']++;
+    sort(vec.begin(), vec.end());
+    int a = vec[25], b = vec[24], c = [23];
+    if(c == 0){
+        cout << -1;
+        return 0;
+    }
+    int res = 3 * c;
+    if(a > c) res++;
+    if(b > c) res++;
+    cout << res;
+}
+using namespace std;
+int main(){
+    int n, k; cin >> n >> k;
+    pair <int, int> twin[n];
+    int arr[k];
+    for(int i = 0; i < n; i++)
+        cin >> twin[i].first >> twin[i].second;
+    for(int i = 0; i < k; i++)
+        cin >> arr[i];
+    sort(twin, twin + n); sort(arr, arr + k);
+    int r = n - 1, res = i = 0;
+    while(r >= 0){
+        if(twin[i].second > 0){
+            res += twin[i].first * arr[r];
+            r--; twin[i].second--;
+        }
+        else
+            i++;
+    }
+    cout << res;
+}
+using namespace std;
+int main(){
+    int n, q; cin >> n >> q;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr, arr + n);
+    for(int i = 0; i < q; i++){
+        int x; cin >> x;
+        int l = 0, r = n - 1;
+        bool flag = 0;
+        while(l < r){
+            int sum = arr[l] + arr[r];
+            if(sum == x){
+                flag = 1; break;
+            }
+            else if(sum < x) l++;
+            else r--;
+        }
+    }
+    (flag) ? cout << "Yes" : cout << "No";
+}
+using namespace std;
+int main() {
+    int n, x, y; cin >> n >> x >> y;
+    int s = abs(x), r = abs(y);
+    int t = s + r;   
+    // Minimum required moves (Manhattan distance)
+    if (t > n) {
+        cout << -1;
+        return 0;
+    }
+    // If the remaining moves (n - t) is odd, we cannot distribute evenly
+    if ((n - t) % 2 != 0) {
+        cout << -1;
+        return 0;
+    }
+    int extraMoves = (n - t) / 2;
+    // Assign moves
+    int a = (y >= 0) ? r + extraMoves : extraMoves;
+    int c = (y < 0) ? r + extraMoves : extraMoves;  
+    int b = (x >= 0) ? s + extraMoves : extraMoves; 
+    int d = (x < 0) ? s + extraMoves : extraMoves; 
+    cout << a << " " << b << " " << c << " " << d << endl;
+    return 0;
+}
+using namespace std;
+int main(){
+    int n, a, b; cin >> n >> a >> b;
+    int lcm = a * b/__gcd(a, b);
+    cout << lcm / a + lcm / b;
+}
+using namespace std;
+int main(){
+    int a, b; cin >> a >> b;
+    int mx = max(a, b), mn = min(a, b);
+    while(mx % mn != 0){
+        int tmp = mx - mn;
+        mx = max(mn, tmp);
+        mn = min(mn, tmp);
+    }
+    cout << mn;
+}
+using namespace std;
+int main(){
+    int n, a, b, c; cin >> n >> a >> b >> c;
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        (x % a == 0 && x % b == 0 && x % c == 0) ? cout << "YA\n" : cout << "No\n";
+    }
+}
+using namespace std;
+int main(){
+    int n, k; cin >> n >> k;
+    int arr[n]; cin >> arr[0];
+    for(int i = 1; i < n; i++){
+        int x; cin >> x;
+        arr[i] = arr[i - 1] + x;
+    }
+    for(int i = 0; i < k; i++){
+        int y; cin >> y;
+        cout << upper_bound(arr, arr + n, y) - arr;
+    }
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    multiset <int> mset;
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        if(mset.lower_bound(x) != mset.end())
+            mset.erase(mset.lower_bound(x));
+        mset.insert(x);
+    }
+    cout << mset.size();
+}
+using namespace std;
+int main(){
+    int n, m; cin >> n >> m;
+    (n % m == 0) ? cout << "Yes" : cout << "No";
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    if(n % 4 == 0) cout << "Yes";
+    else if(n % 2 == 0) cout << "No";
+    else{
+        ((n - 1) % 4 == 0) cout << "No" : cout << "Yes";
+    }
+}
+using namespace std;
+int main(){
+    int n, m; cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            (j % 2 == 0) ? cout << "W" : cout << "B";
+        }
+    }
+}
+using namespace std;
+int main(){
+    int n, m, x, y; cin >> n >> m >> x >> y;
+    int res = (n - x) + (m - y) * 2;
+    (res > 20) ? cout << "Yes": cout << "No";
+}
+using namespace std;
+int main(){
+    int m, a, b; cin >> m >> a >> b;
+    if(a + b == 2 * m){
+        (a % 2 == 1 && b % 2 == 1) ? cout << "1" : cout << "0";
+        cout << min(a, b);
+    }
+    else
+        cout << "0" << " " << min(a, b);
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    cout << (n % 10 ? cout << "Yes": cout << "No");
+}
+using namespace std;
+int main(){
+    int n, cnt = 1; cin >> n;
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        cnt *= x;
+    }
+    cout << cnt;
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    char ch[26];
+    vector <bool> visited(n, false);
+    vector <char> arr(n);
+    vector <int> vec(n), res(n);
+    for(int i = 0; i < 26; i++)
+        ch[i] = 'a' + i;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < 26; j++){
+            if(arr[i] == ch[j]){
+                vec[i] = j + 1;
+                break;
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        int minIdx = -1;
+        for(int j = 0; j < n; j++){
+            if(!visited[j] && (minIdx == - 1 || vec[j] < vec[minIdx]))
+                minIdx = j;
+        }
+        res[minIdx] = n - i;
+        visited[minIdx] = true;
+    }
+    for(int i = 0; i < n; i++)
+        cout << res[i] << " ";
+}
+// output the sequence of unique number
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int arr[1001] = {0}, unique[n], cnt = 0;
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        if(arr[x] == 0){
+            arr[x] = 1;
+            unique[cnt++] = x;
+        }
+    }
+    for(int i = 0; i < cnt; i++)
+        cout << unique[i] << " ";
+}
+using namespace std;
+//find index
+int main(){
+    int n, trgIdx; cin >> n >> trgIdx;
+    int arr[n], cnt = 0;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    if(arr[trgIdx - 1] == 0){
+        for(int i = 0; i < n; i++){
+            if(arr[i] > 0) cnt++;
+        }
+    }
+    else{
+        cnt = trgIdx;
+        for(int i = trgIdx; i < n; i++){
+            if(arr[trgIdx - 1] <= arr[i])
+                cnt++;
+        }
+    }
+    cout << cnt << "\n" << arr[TrgIdx - 1] << "\n";
+    (trgIdx < n) ? cout << arr[trgIdx] : cout << "Idx OutOfBound";
+}
+using namespace std;
+//Findpos
+int main(){
+    int row, col, cnt = 0, mini = INT_MAX, MinRow = -1, MinCol = -1;
+    cin >> row >> col;
+    char arr[row][col];
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j <col; j++)
+            cin >> arr[i][j];
+    }
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            if(arr[i][j] == 'E'){
+                int dist = i + j;
+                if(distance < mini){
+                    //convert to 1-Based Idx
+                    MinRow = i + 1;
+                    MinCol = j + 1;
+                }
+                cnt++;
+            }
+        }
+    }
+    (cnt == 0) ? cout << -1 : cout << MinRow << " " << MinCol;
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    for(int i = n; i > 0; i--){
+        int cnt = 0;
+        for(int j = n; j > 0; j--){
+            for(int k = n; k > 0; k--){
+                if(k * k + j * j == i * i)
+                    cnt++;
+            }
+        }
+        if(cnt == 2){
+            cout << i;
+            return 0;
+        }
+    }
+    cout << -1;
+}
+using namespace std;
+int main(){
+    int a, b, c; cin >> a >> b >> c;
+    int mx = max(a, max(b, c));
+    for(int i = mx; i <= 100; i++){
+        if(i % a != 0 && i % b != 0 && i % c != 0){
+            cout << i;
+            break;
+        }
+    }
+}
+using namespace std;
+int main(){
+    double d; cin >> d;
+    cout << fixed << setprecision(1) << n *(n + 1) /2;
+    int n, m, r; cin >> n >> m >> r;
+    int mn = min(n, m);
+    cout << (mn - 1) / (2 * r + 1) + 1;
+}
+using namespace std;
+int main(){
+    int p, q, r, s, t; cin >> p >> q >> r >> s >> t;
+    int x = sqrt(p * q * r * s * t);
+    cout << x / (q * s) << " " << x / (r * t) << " " << x / (p * s) << " " << x / (q * t) << " " << x / (p * r);
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int arr[n];
+    bool isDecr = false, isIncr = false, isEqual = false;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        if(i > 0){
+            if(arr[i] < arr[i - 1]) isDecr = true;
+            else if(arr[i] > arr[i - 1]) isIncr = true;
+            else isEqual = true;
+        }
+    }
+    if(isIncr && isDecr) cout << "NONE";
+    else if (isEqual){
+        if(isDecr) cout << "NONINCREASING";
+        else if (isIncr) cout << "NONDECREASING";
+        else cout << "EQUAL";
+    }else if (hasIncrease) cout << "NONDECREASING";
+    else if (hasDecrease) cout << "NONINCREASING";
+    else cout << "NONE";
+}
+using namespace std;
+int main(){
+    int n, m; cin >> n >> m;
+    cout << n + (n - 3) - m;
+    int ans = 1;
+    for(int i = 1; i <= n * m - 1; i++)
+        ans *= 2;
+    cout << ans;
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    if(n >= 100)
+        cout << 9;
+    else if(n >= 19 && n <= 99){
+        string str = to_string(n);
+        if(str[1] == '9')
+            cout << n / 10;
+        else
+            cout << n / 10 -1;
+    }
+    else
+        cout << "0";
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int a = b = 0;
+    for(int i = 1; i <= n; i++){
+        int x; cin >> x;
+        if(n > 2){
+            a += x * pow(-1, i % 2);
+            b += x * pow(-1, (i + 1) % 2);
+        }
+        else{
+            b += x * pow(-1, (i + 1) % 2);
+            a = INT_MIN;
+        }
+    }
+    cout << max(a, b);
+}
+using namespace std;
+int main(){
+    int a, b; cin >> a >> b;
+    int arr[a][b];
+    int x, y = z = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            cin >> arr[i][j];
+            if(arr[i][j] == -1) x = (i + j) % 2;
+            else if((i + j) % 2 == 0) y += arr[i][j];
+            else z += arr[i][j];
+        }
+    }
+    if(x == 0)
+        (y > z)? cout << "No\n" : cout << "Yes\n" << z - y;
+    else
+        (y < z)? cout << "No\n" : cout << "Yes\n" << y - z;
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int tmp = 2, cnt = 0;
+    while(tmp <= n)
+        tmp *= 2;
+    cout << tmp - n;
+    int x, y; cin >> x >> y;
+    int mn = min(x, y), mx = max(x, y);
+    cout << (mn - 1) * mx;
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        int a, b; cin >> a >> b;
+        cout << 2 * min(a, b);
+}
+using namespace std;
+int main(){
+    int n, m; cin >> n >> m;
+    int arr[n], cnt = 0;
+    for(int i = 1; i <= n; i++)
+        arr[i] = 0;
+    for(int i = 0; i < m; i++){
+        int x; cin >> x;
+        arr[x] = (arr[x] + 1) % 4;
+    }
+    for(int i = 1; i <= n; i++){
+        if(arr[i] == 2 || arr[i] == 3)
+            cnt++;
+    }
+    cout << cnt;
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int even = odd = 0;
+    for(int i = 0; i < n; i++){
+        int x, y; cin >> x >> y;
+        if((x + y) % 2 == 0) even++;
+        else odd++;
+    }
+    (even == 0 || odd == 0) ? cout << "No" : cout << "Yes";
+}
+using namespace std;
+int main(){
+    string str; cin >> str;
+    int cnt = 0;
+    for(int i = 0; i < str.length(); i++){
+        if(str[i] == 'o') cnt++;
+        if(cnt > 1) break;
+    }
+    (cnt == 1) ? cout << "Yes" : cout << "No";
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    double r, v; cin >> r >> v;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        if(arr[i] == v){
+            cout << "-1";
+            return 0;
+        }
+    }
+    double res = 0;
+    for(int i = 0; i < n; i++)
+        res = max(res, r / abs(arr[i] - v));
+    cout << setprecision(9) << res;
+}
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)   
+        cin >> arr[i];
+    int id = 0;
+    while(id < n && arr[id] == 1)
+        id++;
+    id %= n;
+    int i = id, res = 0, cnt = 0;
+    do{
+        if (arr[i] == 1){
+            cnt++;  
+            res += cnt;
+        }else cnt = 0;
+        i = (i + 1) % n;
+    }while (i != id);
+    cout << res << endl;
+}
+using namespace std;
+int main(){
+    int n, m; cin >> n >> m;
+    int cnt = 1, res = 0;
+    for(int i = 0; i < n; i++){
+        int l, r; cin >> l >> r;
+        if(cnt < l){
+            res += abs(cnt - l);
+            cnt = l;
+        }
+        else if(cnt > r){
+            res += abs(cnt - r);
+            cnt = r;
+        }
+    }
+    cout << res;
+}
