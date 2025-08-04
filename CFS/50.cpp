0e748025a7b16025cc047422b60047b90f1341d8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-08-04 16:54:45 +0200

    Update 50.cpp

--- CFS/50.cpp
+++ CFS/50.cpp
@@ -131,3 +131,22 @@ int main(){
         }
     }
 }
+#include<bits/stdc++.h>
+https://codeforces.com/problemset/problem/4/C
+// 4C. Registration system
+using namespace std;
+int main(){
+    int t; cin >> t;
+    while(t--){
+        string str; cin >> str;
+        map <string, int> mp;
+        if(!mp[str])    cout << "OK" << endl;
+        else    cout << str + to_string(mp[str]);
+        /*
+        if(mp[str] >= 1)    cout << str << mp[str] << endl;
+        else    cout << "Ok" << endl;
+        if(mp[str] == 0)    cout << "Ok" << endl;
+        else    cout << str << mp[str] << endl;    */
+        ++mp[str];
+    }
+}
