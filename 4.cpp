da12316bbcb033fea82eed7c5aedab6388263e4a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-09-03 08:15:52 +0200

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -170,47 +170,6 @@ int main() {
     cout << "\n";
     return 0;
 }
-
-https://codeforces.com/problemset/problem/9/A
-// A. Die Roll
-using namespace std;
-int main(){
-    int a, b; cin >> a >> b;
-    if(a < b)
-        swap(a,b);
-    int c = 0;
-    for(int i = a; i <= 6; i++)
-        c++;
-    int dot = c/6;
-    if(dot == 1)    cout << "1/1" << endl;
-    else{
-        if(c == 0)    cout << "0/1" << endl;
-        else if(c == 1)    cout << "1/6" << endl;
-        else if(c == 2)    cout << "1/3" << endl;
-        else if(c == 3)    cout << "1/2" << endl;
-        else if(c == 4)    cout << "2/3" << endl;
-        else if(c == 5)    cout << "5/6" << endl;
-    }
-    return 0;
-}
-using namespace std;
-int main() {
-    int a, b; cin >> a >> b;
-    int maxVal = max(a, b);
-    int favorable = 6 - maxVal + 1;
-    switch (favorable) {
-        case 0: cout << "0/1"; break;
-        case 1: cout << "1/6"; break;
-        case 2: cout << "1/3"; break;
-        case 3: cout << "1/2"; break;
-        case 4: cout << "2/3"; break;
-        case 5: cout << "5/6"; break;
-        case 6: cout << "1/1"; break;
-    }
-    cout << endl;
-    return 0;
-}
-
 https://codeforces.com/problemset/problem/9/B
 // B. Running Student
 using namespace std;
