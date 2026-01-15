770c3993adaa1a70366c4b8bf6feef1718770075
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-15 23:06:59 +0100

    Update Two Sum.cpp

--- Leetcode/Two Sum.cpp
+++ Leetcode/Two Sum.cpp
@@ -1,3 +1,4 @@
+//1
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -10,5 +11,37 @@ public:
             }
         }
         return {};
+    }
+};
+//860.Lemonade
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        iclassnt n = bills.size() ;
+        int cnt5 = 0, cnt10 = 0, cnt20 = 0;
+        for(int i = 0; i < n; i++){
+            if(cnt5 < 0) return false;
+            if(cnt5 < 0 && (cnt10 <= 0 || cnt20 <= 0)) return false;
+            if(bills[i] == 5)
+                cnt5 += 1;
+            else if(bills[i] == 10){
+                cnt5 -= 1;
+                cnt10 += 1;
+            }
+            else if(bills[i] == 20){
+                cnt20 += 1;
+                if(cnt5 != 0 && cnt10 != 0){
+                    cnt5 -= 1;
+                    cnt10 -= 1;
+                }
+                else if(cnt10 == 0 && cnt5 >= 3)
+                    cnt5 -= 3;
+                else 
+                    return false;
+            }
+        }
+        if(cnt5 < 0) return false;
+        if(cnt5 < 0 && (cnt10 <= 0 || cnt20 <= 0)) return false;
+    return true;
     }
 };
